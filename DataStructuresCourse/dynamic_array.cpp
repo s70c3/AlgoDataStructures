0cd8b7e5089dadcd8c73a85bd3f6de3a7324f878
s70c3 <mari.s70c3@gmail.com>
2023-03-01 00:11:26 +0300

    Several tasks about dyn array;

--- DataStructuresCourse/dynamic_array.cpp
+++ DataStructuresCourse/dynamic_array.cpp
@@ -7,11 +7,31 @@
 int main(void)
 {
     using namespace std;
-    vector<int> ar = {0, -4, 10, 2, -43, 25, 100, 7};
+    //task 1
+    std::vector<int> ar = {0, -4, 10, 2, -43, 25, 100, 7};
 
     for(int i=0; i<ar.size(); i++) {
         cout<<ar[i]<<" ";
     }
+    //task 2
+    vector<char> ar;
+
+    char x;
+    while (!feof(stdin)) {
+        cin >> x;
+        ar.push_back(x);
+    }
+
+    cout<<ar[1]<<" "<<ar[5];
+
+    //task 3
+    std::vector <char> buffer;
+    buffer.reserve(512);
+    string x = "methods: size; push_back;";
+    for(int i=0; i<25; i++) {
+        buffer.push_back(x[i]);
+    }
+    cout<<buffer.size()<<' '<<buffer.capacity();
 
     return 0;
 }
